764ed1bd06c3b4ceca77be07657bb44fd4b15111
Leone Stoico <stoiclionroars@gmail.com>
2023-09-30 18:18:11 +0200

    Fixed bean configuration issue for BookmarkDao.

--- src/main/java/dev/stoiclionroars/bookmarks/configuration/DaoConfiguration.java
+++ src/main/java/dev/stoiclionroars/bookmarks/configuration/DaoConfiguration.java
@@ -1,5 +1,8 @@
 package dev.stoiclionroars.bookmarks.configuration;
 
+import dev.stoiclionroars.bookmarks.business.data.DeleteBookmark;
+import dev.stoiclionroars.bookmarks.business.data.GetAllBookmarks;
+import dev.stoiclionroars.bookmarks.business.data.UpdateBookmark;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +14,21 @@ public class DaoConfiguration {
     
     @Bean 
     AddBookmark addBookmark() {
+        return new BookmarkDao();
+    }
+
+    @Bean
+    GetAllBookmarks getAllBookmarks() {
+        return new BookmarkDao();
+    }
+
+    @Bean
+    UpdateBookmark updateBookmark() {
+        return new BookmarkDao();
+    }
+
+    @Bean
+    DeleteBookmark deleteBookmark() {
         return new BookmarkDao();
     }
 }
