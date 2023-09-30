0b6bd3c126a284011737f519dbfc809d4c3ca1a1
Leone Stoico <stoiclionroars@gmail.com>
2023-09-30 17:01:19 +0200

    Added table and domain class for Workspace Entity.

---
+++ src/main/java/dev/stoiclionroars/bookmarks/business/domain/Workspace.java
@@ -0,0 +1,31 @@
+package dev.stoiclionroars.bookmarks.business.domain;
+
+import java.lang.invoke.StringConcatFactory;
+import java.util.UUID;
+
+public class Workspace {
+
+    private UUID id;
+
+    private String name;
+
+    private String description;
+
+    public Workspace(UUID id, String name, String description, String link) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public UUID getId(){
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
